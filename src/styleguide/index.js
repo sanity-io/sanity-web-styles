c1679d0a709b2e2b154e6288c5a0bdf012b473d9
Marius Lundgård <studio@mariuslundgard.com>
2019-03-27 09:47:59 +0100

    Update styleguide

--- src/styleguide/index.js
+++ src/styleguide/index.js
@@ -1,3 +1,7 @@
 const pkg = require("../../package.json");
 
 document.getElementById("version").innerHTML = "v" + pkg.version;
+document.getElementById("description").innerHTML = pkg.description.replace(
+  "Sanity.io",
+  '<a href="https://www.sanity.io/" target="_blank">Sanity.io</a>'
+);
